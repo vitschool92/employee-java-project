4fe7ab5a76411491370fc30fe2b6fdbac65b6620
vitschool92 <vitschool92@gmail.com>
2023-12-01 11:29:11 +0530

    Added the arithmetic file

---
+++ src/vit/xyz/emp/Arithmetic.java
@@ -0,0 +1,14 @@
+package vit.xyz.emp;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+	
+		int a=10;
+		int b=20;
+		System.out.println("A+b="+(a+b));
+		System.out.println("A - b="+(a-b));
+		System.out.println("A * b="+(a*b));
+	}
+
+}
--- src/vit/xyz/emp/First.java
+++ src/vit/xyz/emp/First.java
@@ -5,5 +5,4 @@ public class First {
 	public static void main(String[] args) {
 		System.out.println("Hi This is employee project");
 	}
-
 }
