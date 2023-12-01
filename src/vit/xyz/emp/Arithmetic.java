032762f406a44de2b62a779592f7d94ecab778de
vitschool92 <vitschool92@gmail.com>
2023-12-01 11:32:11 +0530

    removed spaces

--- src/vit/xyz/emp/Arithmetic.java
+++ src/vit/xyz/emp/Arithmetic.java
@@ -3,12 +3,10 @@ package vit.xyz.emp;
 public class Arithmetic {
 
 	public static void main(String[] args) {
-	
 		int a=10;
 		int b=20;
-		System.out.println("A+b="+(a+b));
-		System.out.println("A - b="+(a-b));
-		System.out.println("A * b="+(a*b));
+		System.out.println("a + b="+(a+b));
+		System.out.println("a - b="+(a-b));
+		System.out.println("a * b="+(a*b));
 	}
-
 }
