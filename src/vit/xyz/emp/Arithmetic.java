5216c2c7f4a59016ad21ef34f1525c48ac093c23
VITech Talks <152469405+vitschool92@users.noreply.github.com>
2023-12-01 11:35:21 +0530

    division logic

--- src/vit/xyz/emp/Arithmetic.java
+++ src/vit/xyz/emp/Arithmetic.java
@@ -8,5 +8,6 @@ public class Arithmetic {
 		System.out.println("a + b="+(a+b));
 		System.out.println("a - b="+(a-b));
 		System.out.println("a * b="+(a*b));
+		System.out.println("a / b="+(a/b));
 	}
 }
