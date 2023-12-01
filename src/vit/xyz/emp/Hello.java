1f3a10adcccc77bc0cce5799943471d901bdb993
vitschool92 <vitschool92@gmail.com>
2023-12-01 11:57:26 +0530

    "Adding the logic for employee salary"

---
+++ src/vit/xyz/emp/Hello.java
@@ -0,0 +1,78 @@
+package vit.xyz.emp;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	     System.out.println("------------hello world program------");
+	       System.out.println("hellow world");
+	       System.out.println("hello , hii git hub");
+		       
+//	         System.out.println("-----------Arthematic operators-------");
+//	         int a = 10;
+//	         int b = 20;
+//	         System.out.println("sum of the two numbers ="+(a+b));
+//	         System.out.println("sub of the two numbers ="+(a-b));
+//	         System.out.println("mul of the two numbers ="+(a*b));
+//	         System.out.println("div of the two numbers ="+(a/b));
+//	         System.out.println("Reminder of the two numbers ="+(a%b));
+//	         System.out.println("----------------get the input from end user-------");
+	         Scanner sc =  new Scanner(System.in);
+//	         System.out.println("Enter Name,Age and Salary");
+//	         
+//	         String name = sc.nextLine();
+//	         int age = sc.nextInt();
+//	         double salary =sc.nextDouble();
+//	         
+//	         System.out.println("Name = "+ name);
+//	         System.out.println("Age = "+ age);
+//	         System.out.println("Salary="+salary);
+//	         
+//	         System.out.println("----PTR/100-----");
+//	         
+//	         System.out.println("Enter the principle Amount");
+//	         double principleAmount = sc.nextDouble();
+//	         
+//	         System.out.println("Enter time period");
+//	         int time = sc.nextInt();
+//	         
+//	         System.out.println("Enter rate of Interest");
+//	         int interestRate= sc.nextInt();
+//	         
+//	         double result = (principleAmount * time * interestRate)/100;
+//	         
+//	         System.out.println("Simple interest ="+ result);
+//	         
+//	         System.out.println("-------------positive/negative----------");
+//	         
+//	         System.out.println("enter any number");
+//	         int number = sc.nextInt();
+//	          
+//	         if(number > 0) {
+//	        	 System.out.println("Given number is postive");
+//	         }
+//	         else {
+//	        	 System.out.println("Given number is negative");
+//	        	 
+//	         }
+//	         
+//	         System.out.println("--------vote------");
+//	         System.out.println("Enter Name and Age");
+//	         
+//	         String Name = sc.nextLine();
+//	         int Age = sc.nextInt();
+//	         
+//	         if(Age >=18) {
+//	        	 System.out.println(Name +"Is Eligible for vote");
+//	         }
+//	         else {
+//	        	 System.out.println(Name +"Is Not Eligible for vote");
+//	         }
+	         
+	         
+		}
+
+	}
+
+
